445a6167a51db15da535c9593a01b67fc1c57df3
JulienM <julienm@marmelab.com>
2022-04-16 10:57:02 +0200

    refacto translations

--- translate.js
+++ translate.js
@@ -1,72 +1,68 @@
 import fruits from "./assets/fruits.js";
 import { default_config } from "./config/config.js";
 
-export const setMirabelle = (arg) => {
-  const toReplaceFruit = default_config.fruit;
-  if (typeof arg === "object") {
-    const newObject = Object.assign({}, arg);
+const applyTranslateByType = (source, translate) => {
+  if (typeof source === "object") {
+    if (source instanceof Date) {
+      return source;
+    }
+    const newObject = Object.assign({}, source);
     Object.keys(newObject).forEach((key) => {
       if (typeof newObject[key] === "string") {
-        fruits.map((fruit) => {
-          const fruitRegExp = new RegExp(fruit, "ig");
-          newObject[key] = newObject[key].replace(fruitRegExp, toReplaceFruit);
-        });
+        newObject[key] = translate(newObject[key]);
       }
     });
     return newObject;
   }
-  if (typeof arg === "string") {
+  if (typeof source === "string") {
+    return translate(source);
+  }
+  return source;
+};
+
+export const setMirabelle = (source) => {
+  const toReplaceFruit = default_config.fruit;
+
+  const replaceFunction = (text) => {
+    let newText = `${text}`;
     fruits.map((fruit) => {
       const fruitRegExp = new RegExp(fruit, "ig");
-      arg = arg.replace(fruitRegExp, toReplaceFruit);
+      newText = newText.replace(fruitRegExp, toReplaceFruit);
     });
-  }
-  return arg;
+    return newText;
+  };
+
+  return applyTranslateByType(source, replaceFunction);
 };
 
-const regexCapital = /\b[A-Z][a-zA-Z]*\b/;
-
-export const setLe = (arg) => {
+export const setLe = (source) => {
   const le = default_config.le;
-  if (typeof arg === "object") {
-    const newObject = Object.assign({}, arg);
-    Object.keys(newObject).forEach((key) => {
-      if (typeof newObject[key] === "string") {
-        newObject[key] = newObject[key].replace(regexCapital, `${le}$&`);
-      }
-    });
-    return newObject;
-  }
-  return arg.replace(regexCapital, `${le}$&`);
+  const regexCapital = /\b[A-Z][a-zA-Z]*\b/;
+
+  const replaceFunction = (text) => {
+    return text.replace(regexCapital, `${le}$&`);
+  };
+
+  return applyTranslateByType(source, replaceFunction);
 };
 
-export const setGros = (arg) => {
+export const setGros = (source) => {
   const gros = default_config.suffix;
-  if (typeof arg === "object") {
-    const newObject = Object.assign({}, arg);
-    Object.keys(newObject).forEach((key) => {
-      newObject[key] = newObject[key] + gros;
-    });
-    return newObject;
-  }
-  return arg + gros;
+
+  const replaceFunction = (text) => {
+    return text + gros;
+  };
+
+  return applyTranslateByType(source, replaceFunction);
 };
 
-export const setO = (arg) => {
+export const setO = (source) => {
   const toReplaceA = default_config.a;
   const regexpToFindA = new RegExp(/[aAâ]/, "g");
-  if (typeof arg === "object") {
-    const newObject = Object.assign({}, arg);
-    Object.keys(newObject).forEach((key) => {
-      if (typeof newObject[key] === "string") {
-        newObject[key] = newObject[key].replace(regexpToFindA, toReplaceA);
-      }
-    });
-    return newObject;
-  }
-  if (typeof arg === "string") {
-    arg = arg.replace(regexpToFindA, toReplaceA);
-    return arg;
-  }
-  return arg;
+
+  const replaceFunction = (text) => {
+    return text.replace(regexpToFindA, toReplaceA);
+  };
+
+  return applyTranslateByType(source, replaceFunction);
 };
