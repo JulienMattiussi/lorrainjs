6103de5b0b09aa42c19390281c0c17904f22474e
JulienM <julienm@marmelab.com>
2022-04-22 14:08:54 +0200

    clean

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,18 @@ import {
   originalWarn,
   originalError,
   translate,
-} from "./logger.js";
+} from "./logger";
 
-export { initAll } from "./logger.js";
-export { initLog } from "./logger.js";
-export { initWarn } from "./logger.js";
-export { initError } from "./logger.js";
-export { originalLog } from "./logger.js";
-export { originalWarn } from "./logger.js";
-export { originalError } from "./logger.js";
-export { translate } from "./logger.js";
+export {
+  initAll,
+  initLog,
+  initWarn,
+  initError,
+  originalLog,
+  originalWarn,
+  originalError,
+  translate,
+} from "./logger";
 
 export default {
   initAll,
